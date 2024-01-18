2e32a5f62780eaf73c50084dbf87b7a49c805f0f
Laisses <laisse@gmail.com>
2024-01-18 16:32:19 -0300

    feat: Adiciona implementação de fixedPermutations()

--- perm.mjs
+++ perm.mjs
@@ -1,2 +1,9 @@
-export const fixedPermutations = () => {};
+export const fixedPermutations = (s, n) => {
+	let res = [];
+	for (let i = 0; i < s.length - n + 1; i++) {
+		res.push(s.substring(i, i + n));
+	}
+	return res;
+};
+
 export const permutations = () => {};
